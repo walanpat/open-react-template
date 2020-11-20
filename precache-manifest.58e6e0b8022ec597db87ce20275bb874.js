1911d4640e24fbddfa9504a07c926e82d070b3a2
walanpat <39136284+walanpat@users.noreply.github.com>
2020-11-20 14:14:02 -0500

    Updates

---
+++ precache-manifest.58e6e0b8022ec597db87ce20275bb874.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68c0f151ff1c7d267a6f27f09a2d624",
+    "url": "/open-react-template/index.html"
+  },
+  {
+    "revision": "5b034b451ecce91537ea",
+    "url": "/open-react-template/static/css/main.e7600209.chunk.css"
+  },
+  {
+    "revision": "705e36e7073b8a76cc23",
+    "url": "/open-react-template/static/js/2.6b233028.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/open-react-template/static/js/2.6b233028.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b034b451ecce91537ea",
+    "url": "/open-react-template/static/js/main.206161d8.chunk.js"
+  },
+  {
+    "revision": "99472f060f7b5947f713",
+    "url": "/open-react-template/static/js/runtime-main.569e6db8.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/open-react-template/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/open-react-template/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/open-react-template/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/open-react-template/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/open-react-template/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/open-react-template/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/open-react-template/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/open-react-template/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/open-react-template/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/open-react-template/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/open-react-template/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/open-react-template/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "a5b364c4fad6ca609517c413c83c195b",
+    "url": "/open-react-template/static/media/j.a5b364c4.jpg"
+  },
+  {
+    "revision": "94a58fb37cd1ab6ae32e1157f97d513a",
+    "url": "/open-react-template/static/media/studentlogo.94a58fb3.png"
+  }
+]);
